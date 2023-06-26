ca6929ec3eeb660fc04db17f6b95f06a1fdbdd89
Justin Tran <jus81818@yahoo.com>
2023-06-26 12:06:14 -0400

    implementing volume

--- src/Timer.js
+++ src/Timer.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "./Timer.css"; 
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import SettingsIcon from '@mui/icons-material/Settings';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
 
 function Timer() {
     const [pomoTime, setpomoTime] = useState({min: 25, sec: 0, decreasing: false, default: true}); 
@@ -13,24 +15,36 @@ function Timer() {
     const [cycle, setCycle] = useState(0); 
     const [automatic, setAutomatic] = useState(false); 
     const [customize, setCustomize] = useState(false); 
+    const [mute, setMute] = useState(false); 
 
+    // to render the time display for the first time page is loaded or when it is decreasing in time  
     useEffect(() => {
         if (time.decreasing || time.default) {
             setDisplay(time.min.toString().padStart(2, '0') + ":" + time.sec.toString().padStart(2, '0')); 
         } 
     }, [time]);
 
+    // to render the timer for each mode the client selects and certain constraints are put 
     useEffect(() => {
         if (mode === "pomodoro" && pomoTime.min > 0 && pomoTime.min < 361) {
             setTime(pomoTime); 
         } else if (mode === "short" && shortTime.min > 0 && shortTime.min < 361) {
             setTime(shortTime); 
-        } else if (mode === "long") {
+        } else if (mode === "long" && longTime.min > 0 && longTime.min < 361) {
             setTime(longTime); 
         }
     }, [pomoTime, shortTime, longTime]); 
 
     useEffect(() => {
+        if (time.decreasing) {
+            document.title = display + " | Sone"; 
+        } else {
+            document.title = "Sone"; 
+        }
+    }, [display])
+
+    useEffect(() => {
+        // to make mode switching deterministic based on cycles 
         if (time.min === 0 && time.sec === 0) {
             if (mode === "pomodoro") {
                 if (cycle !== 0 && cycle % 4 === 0) {
@@ -44,20 +58,22 @@ function Timer() {
             } 
             if (automatic) setTime((oldTime) => {return {...oldTime, decreasing: true}})
         }
-
+        
+        // main functionality for the timer 
         if (time.decreasing && time.min >= 0 && time.sec >= 0) {
             const timeoutID = setTimeout(() => {
                 if (time.sec === 0) {   
                     setTime((oldTime) => {return {...oldTime, min: oldTime.min-1, sec: 60}})
                 }
                 setTime((oldTime) => {return {...oldTime, sec: oldTime.sec-1}})
-            }, 1)
+            }, 1000)
             return () => {
                 clearTimeout(timeoutID); 
             }
         }       
     }, [time]) 
 
+    // when the user hits start or pause 
     const handleTime = () => {
         if (!time.decreasing) {
             return setTime((oldTime) => {return {...oldTime, decreasing: true, default: false}})
@@ -66,6 +82,7 @@ function Timer() {
         }
     }
 
+    // when the user clicks on other modes 
     const handleMode = (str) => {
         if (str === "pomodoro") {
             setMode("pomodoro"); 
@@ -79,6 +96,7 @@ function Timer() {
         }
     }
 
+    // when the user clicks reset 
     const handleReset = () => {
         if (mode === "pomodoro") {
             setTime(pomoTime);
@@ -90,13 +108,18 @@ function Timer() {
     }
 
     const handleOptions = () => {
-        setCustomize((oldCustomize) => {return !oldCustomize}) 
+        setCustomize((oldCustomize) => {return !oldCustomize});
     }
 
     const handleAutomatic = () => {
-        setAutomatic((oldAutomatic) => {return !oldAutomatic})
+        setAutomatic((oldAutomatic) => {return !oldAutomatic});
     }
 
+    const handleMute = () => {
+        setMute((oldMute) => {return !oldMute});
+    }
+
+    // used for the input to change based off of what user enters
     const handleChange = (e) => {
         const name = e.target.name; 
         const value = e.target.value; 
@@ -110,9 +133,16 @@ function Timer() {
         }
     }
 
+    const handleVolume = (e) => {
+        if (mute) {
+            e.value = 0; 
+        }
+    }
+
     return ( 
-        <div className="main">
+        <div className="main">  
             <div className={customize ? "expandContainer" : "container"}>
+                {/* top component with the display and start and restart button*/}
                 <div className="top">
                     <div className="time">
                         {display}
@@ -125,6 +155,7 @@ function Timer() {
                     <RestartAltIcon className="reset" onClick={() => handleReset()}/>
                 </div>
 
+                {/* bottom component with the modes and options */}
                 <div className="bot">
                     <button className="pomodoro" onClick={() => handleMode("pomodoro")}>
                         Pomodoro
@@ -141,6 +172,7 @@ function Timer() {
                     <SettingsIcon className="settings" onClick={() => handleOptions()}/>
                 </div>
             </div>
+            {/* settings tab */}
             <div className={customize ? "showSettings" : "hideSettings"}>
                 <div className="clickers">
                     <input className="check" type="checkbox" 
@@ -186,6 +218,17 @@ function Timer() {
                             onChange={handleChange}
                         />
                     </div>
+                </div>
+
+                <div className="volume"> 
+                    {mute ? <VolumeOffIcon className="volume-icon" onClick={() => handleMute()}/> : 
+                    <VolumeUpIcon className="volume-icon" onClick={() => handleMute()}/> }
+
+                    <input 
+                        className="volume-adjust"
+                        type="range"
+                        onChange={handleVolume}
+                    />
                 </div>
 
                 <button 
